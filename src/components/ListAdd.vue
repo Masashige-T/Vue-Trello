365b500a51d36a4ae62b1dd0e9157d60e75a8e54
Masashige-T <m.t0719@outlook.jp>
2020-08-31 22:40:58 +0900

    add: 入力に応じてサブミットボタンの活性化

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,10 +1,17 @@
 <template>
   <!-- @ = v-onの省略形 -->
   <!-- .pervent = perventDefaultと同義 submitイベントでのリロードを中断 -->
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <!-- v-modelで指定したdataと紐付け(双方向バインディング？) -->
-    <input v-model="title" type="text" class="text-input" placeholder="Add new list" />
-    <button type="submit" class="add-button">Add</button>
+    <input
+      v-model="title"
+      type="text"
+      class="text-input"
+      placeholder="Add new list"
+      @focusin="startEditing"
+      @focusout="finishEditing"
+    />
+    <button type="submit" class="add-button" v-if="isEditing || titleExists">Add</button>
   </form>
 </template>
 
@@ -13,13 +20,35 @@ export default {
   data: function () {
     return {
       title: "",
+      isEditing: false,
     };
+  },
+  computed: {
+    classList() {
+      const classList = ["addlist"];
+      if (this.isEditing) {
+        classList.push("active");
+      }
+      if (this.titleExists) {
+        classList.push("addable");
+      }
+      return classList;
+    },
+    titleExists() {
+      return this.title.length > 0;
+    },
   },
   methods: {
     // actionをdispatchしてtitleを空に
     addList: function () {
       this.$store.dispatch("addlist", { title: this.title });
       this.title = "";
+    },
+    startEditing: function () {
+      this.isEditing = true;
+    },
+    finishEditing: function () {
+      this.isEditing = false;
     },
   },
 };
