ae0b25b0c33adf8564af70cbf9bbfddcbe507a2f
Masashige-T <m.t0719@outlook.jp>
2020-09-04 14:59:03 +0900

    add: Localstorageにあるタスクを表示するListを表示するように

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,17 +3,31 @@
     <header>my Trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <div class="list-index">
+        <List
+          v-for="(item, index) in lists"
+          :key="item.id"
+          :title="item.title"
+          :listIndex="index"
+        />
+      </div>
       <ListAdd />
     </main>
   </div>
 </template>
 
 <script>
-import ListAdd from "./ListAdd";
+import ListAdd from "./ListAdd"
+import List from "./List"
+import { mapState } from "vuex"
 
 export default {
   components: {
     ListAdd,
+    List,
   },
-};
+  computed: {
+    ...mapState(["lists"]),
+  },
+}
 </script>
